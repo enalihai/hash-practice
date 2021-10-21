c49761b64d92e30d390c62a6e718b39e11578362
Luke Pascale <lpluke5@gmail.com>
2021-10-20 20:20:16 -0600

    Add model of dog in hash syntax

--- hash-examples.rb
+++ hash-examples.rb
@@ -5,3 +5,11 @@ car = {
   'miles_per_year' => 12000'
   'is_safe' => true
 }
+
+dog = {
+  'is_k9' = true
+  'likes_bacon' => true
+  'is_furry' => true
+  'likes_pets' => true
+  'name' => "Oakley"
+}
