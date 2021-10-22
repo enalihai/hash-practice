f3325207eeab9ad2b1239480113851c6b38bd906
Luke Pascale <lpluke5@gmail.com>
2021-10-22 12:19:17 -0600

    Add missing syntax , to code.

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,24 +1,24 @@
 car = {
-  favorite_brand:'Mercedes'
-  current_brand:'Tacoma'
-  'miles_per_year' => 12000
-  'is_safe' => true
-  'color' =>
+  favorite_brand:'Mercedes',
+  current_brand:'Tacoma',
+  'miles_per_year' => 12000,
+  'is_safe' => true,
+  'color' => 'Blue'
 }
 
 
 dog = {
-  'is_k9' => true
-  'likes_bacon' => true
-  'is_furry' => true
-  'likes_pets' => true
-  'name' => "Oakley"
-  'age' => 5
+  'is_k9' => true,
+  'likes_bacon' => true,
+  'is_furry' => true,
+  'likes_pets' => true,
+  'name' => "Oakley",
+  'age' => 5,
 }
 
 
 customer = {
-  'has_money' => true
-  'is_interested' => true
+  'has_money' => true,
+  'is_interested' => true,
 
 }
